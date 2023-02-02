1d9fe87966e5018243d54561355b91d51fd8f95a
wolfbiters <the.wolf.biters@gmail.com>
2023-02-02 12:37:10 -0500

    clean up

--- src/blinky.adb
+++ src/blinky.adb
@@ -40,28 +40,18 @@
 --  boards, and indeed defines a different number of LEDs on different boards.
 --  The gpr file determines which board is actually used.
 
-with Last_Chance_Handler;  pragma Unreferenced (Last_Chance_Handler);
---  The "last chance handler" is the user-defined routine that is called when
---  an exception is propagated. We need it in the executable, therefore it
---  must be somewhere in the closure of the context clauses.
-
 with STM32.Board;   use STM32.Board;
 
 with STM32.GPIO;    use STM32.GPIO;
 with Ada.Real_Time; use Ada.Real_Time;
 
 procedure Blinky is
-
    Period : constant Time_Span := Milliseconds (1000);  -- arbitrary
-
    Next_Release : Time := Clock;
-
 begin
    STM32.Board.Initialize_LEDs;
-
    loop
       Toggle (All_LEDs);
-
       Next_Release := Next_Release + Period;
       delay until Next_Release;
    end loop;
